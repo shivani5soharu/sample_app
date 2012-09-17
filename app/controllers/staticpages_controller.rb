276e06d8ec4a8b94bab4621f359f1338f05e2480
shivani <shivani@seasiaconsulting.com>
2012-09-17 15:38:19 +0530

    red-to-green-testing done

--- app/controllers/staticpages_controller.rb
+++ app/controllers/staticpages_controller.rb
@@ -1,7 +1,12 @@
 class StaticpagesController < ApplicationController
+
   def home
   end
 
   def help
   end
+  
+  def about
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ SampleApp::Application.routes.draw do
   get "staticpages/home"
 
   get "staticpages/help"
-
+  
+  get "staticpages/about"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
