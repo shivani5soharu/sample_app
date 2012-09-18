bd6045f082f93fbef8802aa0d2a4c611028b1dc3
shivani <shivani@seasiaconsulting.com>
2012-09-18 15:29:44 +0530

    testing withou error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,5 @@
 module ApplicationHelper
 
-<<<<<<< HEAD
-  # Returns the full title on a per-page basis.
-=======
- # Returns the full title on a per-page basis.
->>>>>>> filling-in-layout
   def full_title(page_title)
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -58,10 +58,9 @@ describe "StaticPages" do
     it "should have the title 'About Us'" do
        visit '/staticpages/about'
        page.should have_selector('title', :text => "#{base_title}")
-<<<<<<< HEAD
+
     end
-=======
->>>>>>> filling-in-layout
+
 
     it "should not have a custom page title" do
         visit '/staticpages/about'
@@ -82,7 +81,7 @@ describe "StaticPages" do
     it "should have the title 'Contact'" do
        visit '/staticpages/contact'
        page.should have_selector('title', :text => "#{base_title}")
-<<<<<<< HEAD
+
 
     end  
 
@@ -90,8 +89,7 @@ describe "StaticPages" do
         visit '/staticpages/contact'
         page.should_not have_selector('title', :text => '| Contact')
     end
-=======
->>>>>>> filling-in-layout
+
 
  
   end
